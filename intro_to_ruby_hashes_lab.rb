08dc43ccace1b70f8b99926a1d25fb2c237254f3
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-22 16:10:20 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,25 +19,7 @@ def monopoly_with_second_tier
   {
     :pieces=>4,
     :rent_in_dollars => {}
-    :names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
-    }
+    :names => {}
   }
 }
 end
